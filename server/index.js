ffd01290b8d841fde6e88b7d72fd60b016afacfd
Chitroju Devendra Chakrieswar <devendrachitroju555@gmail.com>
2025-03-20 00:06:45 +0530

    tested apis endpoints on database

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const codingRoutes = require('./routes/test');
+
+const app = express();
+app.use(cors());
+app.use('/api', codingRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
